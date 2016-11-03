172054b8294d7193003b7a3a507114e5a9224f39
JackMasterson <jackmasterson5@gmail.com>
2016-11-03 11:48:43 -0400

    change to djangocms-installer

--- kiddify/urls.py
+++ kiddify/urls.py
@@ -1,15 +1,29 @@
-from django.conf.urls import url
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
 
-from . import views
+from cms.sitemaps import CMSSitemap
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+admin.autodiscover()
 
 urlpatterns = [
-	url(r'^$', views.main_list, name='main_list'),
-    # ex: /polls/
-  #  url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
- #   url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /polls/5/results/
- #   url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /polls/5/vote/
- #   url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
-] 
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
+        {'sitemaps': {'cmspages': CMSSitemap}}),
+    url(r'^select2/', include('django_select2.urls')),
+]
+
+urlpatterns += i18n_patterns('',
+    url(r'^admin/', include(admin.site.urls)),  # NOQA
+    url(r'^', include('cms.urls')),
+)
+
+# This is only needed when using runserver.
+if settings.DEBUG:
+    urlpatterns = [
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        ] + staticfiles_urlpatterns() + urlpatterns
