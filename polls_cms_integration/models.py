514fb28db60ceea0382b052477ee6c615d463693
JackMasterson <jackmasterson5@gmail.com>
2016-11-12 07:40:56 -0500

    adjust textbox strings for clarity

--- polls_cms_integration/models.py
+++ polls_cms_integration/models.py
@@ -4,15 +4,15 @@ from django.db import models
 from django.utils import timezone
 
 class VidInfo(models.Model):
-    avatar_img = models.CharField(max_length=300)
-    title = models.CharField(max_length=200)
-    country_flag = models.CharField(max_length=200)
-    vid_poster = models.CharField(max_length=200)
-    vid_src = models.CharField(max_length=200)
-    src_type = models.CharField(max_length=200, default="some string")
-    track_kind_first=models.CharField(max_length=200, default="some string")
-    track_kind_second=models.CharField(max_length=200, default="some string")
-    thumb_img = models.CharField(max_length=200)
+    avatar_img = models.CharField(max_length=300, default="Avatar Image src")
+    title = models.CharField(max_length=200, default="Title of the talk")
+    country_flag = models.CharField(max_length=200, default="Country Flag src")
+    vid_poster = models.CharField(max_length=200, default="Vid Poster jpg")
+    vid_src = models.CharField(max_length=200, default="Video src")
+    src_type = models.CharField(max_length=200, default="Source src mp4")
+    track_kind_first=models.CharField(max_length=200, default="First Track src")
+    track_kind_second=models.CharField(max_length=200, default="Second Track src")
+    thumb_img = models.CharField(max_length=200, default="Thumb image src")
 
     def __str__(self):
       return(self.title)
